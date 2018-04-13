72ffcaa7b4b9d6d596b8190f61619e6b69872ac4
spirits3 <flo_diablo@hotmail.com>
2018-04-13 11:01:54 +0200

    ajout de la classe produit

---
+++ produit.cpp
@@ -0,0 +1,82 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : Produit.cpp
+ Auteur(s)   : Basile Botebol, Schaufelberger Florian, Nicodème Stalder
+ Date        : 10.04.2018
+
+ But         : Implémentation de la classe Produit qui contient : 
+ * - un champs privé no de type unsigned int, le numero du produit
+ * - un champs privé nom, de type string, le nom du produit
+ * - un champs privé prix de type double, le prix du produit
+ * - un constructeur (publique) à trois paramètres qui initialise dans cet ordre 
+ le no, le nom et le prix 
+ * - les surcharges d'opérateur (publiques) -=, +=, *= et /= avec un double
+ qui modifie le prix du produit 
+ * - les fonctions amies de surcharge d'opérateur +, -, * et / avec un double qui 
+ modifient également seulement le prix
+ * - les fonction amies de surcharge d'opérateur == et =!, qui comparent strictement
+ champs à champs les deux objets.
+
+ Remarque(s) : certaines surcharges d'opérateu sont peut être superflues, mais 
+ néanmoins implémentées, en prévision de la suite
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+#include "Produit.h"
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+ostream& operator<<(ostream& lhs, Produit rhs) {
+    return lhs << '(' << rhs.no << ", \"" << rhs.nom << "\", " << rhs.prix << ')';
+}
+
+Produit operator+(Produit lhs, const double& rhs) {
+    return lhs += rhs;
+}
+
+Produit operator-(Produit lhs, const double& rhs) {
+    return lhs -= rhs;
+}
+
+Produit operator*(Produit lhs, const double& rhs) {
+    return lhs *= rhs;
+}
+
+Produit operator/(Produit lhs, const double& rhs) {
+    return lhs /= rhs;
+}
+
+bool operator==(const Produit& lhs, const Produit& rhs) {
+    return (lhs.no == rhs.no && lhs.nom == rhs.nom && rhs.prix == lhs.prix);
+}
+
+bool operator!=(const Produit& lhs, const Produit& rhs) {
+    return !(lhs == rhs);
+}
+
+Produit::Produit(unsigned int no, std::string nom, double prix) : no(no), nom(nom), prix(prix) {
+}
+
+Produit& Produit::operator+=(double rhs) {
+    prix += rhs;
+    return *this;
+}
+
+Produit& Produit::operator-=(double rhs) {
+    prix -= rhs;
+    return *this;
+}
+
+Produit& Produit::operator*=(double rhs) {
+    prix *= rhs;
+    return *this;
+}
+
+Produit& Produit::operator/=(double rhs) {
+    prix /= rhs;
+    return *this;
+}
--- produit.h
+++ produit.h
@@ -1,17 +1,58 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : Produit.h
+ Auteur(s)   : Basile Botebol, Schaufelberger Florian, Nicodème Stalder
+ Date        : 10.04.2018
+
+ But         : Déclaration de la classe Produit qui contient : 
+ * - un champs privé no de type unsigned int, le numero du produit
+ * - un champs privé nom, de type string, le nom du produit
+ * - un champs privé prix de type double, le prix du produit
+ * - un constructeur (publique) à trois paramètres qui initialise dans cet ordre 
+ le no, le nom et le prix 
+ * - les surcharges d'opérateur (publiques) -=, +=, *= et /= avec un double
+ qui modifie le prix du produit 
+ * - les fonctions amies de surcharge d'opérateur +, -, * et / avec un double qui 
+ modifient également seulement le prix
+ * - les fonction amies de surcharge d'opérateur == et =!, qui comparent strictement
+ champs à champs les deux objets.
+
+ Remarque(s) : certaines surcharges d'opérateu sont peut être superflues, mais 
+ néanmoins implémentées, en prévision de la suite
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
 #ifndef PRODUIT_H
 #define PRODUIT_H
-
 #include <string>
+#include <ostream>
 
 class Produit {
+    friend std::ostream& operator<<(std::ostream& lhs, Produit rhs);
+    friend Produit operator+(Produit lhs, const double& rhs);
+    friend Produit operator-(Produit lhs, const double& rhs);
+    friend Produit operator*(Produit lhs, const double& rhs);
+    friend Produit operator/(Produit lhs, const double& rhs);
+    friend bool operator==(const Produit& lhs, const Produit& rhs);
+    friend bool operator!=(const Produit& lhs, const Produit& rhs);
 
 public:
-	Produit(int numero, std::string libbele, double prix);
+
+    Produit(unsigned int no, std::string nom, double prix);
+    Produit& operator+=(double rhs);
+    Produit& operator-=(double rhs);
+    Produit& operator*=(double rhs);
+    Produit& operator/=(double rhs);
 
 private:
-	int numero;
-	std::string libbele;
-	double prix;
+
+    unsigned int no;
+    std::string nom;
+    double prix;
 };
 
-#endif
+
+#endif /* PRODUIT_H */
+
